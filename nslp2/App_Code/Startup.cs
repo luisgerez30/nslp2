2cbb6cede7f047d951dd9ea9a120b4562dd8ec40
luisgerez30 <42754358+luisgerez30@users.noreply.github.com>
2021-06-03 05:49:00 -0300

    Agregar archivos de proyecto.

---
+++ nslp2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nslp2.Startup))]
+namespace nslp2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
